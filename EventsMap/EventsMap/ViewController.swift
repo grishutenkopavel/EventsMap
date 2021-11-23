6c83e8998713ea6590665e52f5ed776e8e726bfe
Pavel Grishutenko <grishutenkopp@gmail.com>
2021-11-23 23:39:15 +0300

    start project

---
+++ EventsMap/EventsMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsMap
+//
+//  Created by Павел Гришутенко on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
